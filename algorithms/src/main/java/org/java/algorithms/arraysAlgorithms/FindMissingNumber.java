00e61937e3e6a431ae199307256b4d31f0bcd9cf
Mohammed_Elaouri <mohammedelaouri@gmail.com>
2024-03-19 17:28:38 +0000

    create a new algorithm to resize an array and an other alogorithm to serch

---
+++ algorithms/src/main/java/org/java/algorithms/arraysAlgorithms/FindMissingNumber.java
@@ -0,0 +1,15 @@
+package org.java.algorithms.arraysAlgorithms;
+
+public class FindMissingNumber {
+    /**
+     * Given an array of n-1 distinct numbers in the range of 1 to n .
+     * find the missing number in it
+     * arr[]: {2,4,1,8,6,3,7}
+     * output: 5
+     * Explanation :the missing number from range 1 to 8 is 5
+     */
+    public static int searchForMissingNumbers(int[] arr , int n){
+
+        return 0;
+    }
+}
--- algorithms/src/main/java/org/java/algorithms/arraysAlgorithms/ResizeArray.java
+++ algorithms/src/main/java/org/java/algorithms/arraysAlgorithms/ResizeArray.java
@@ -1,4 +1,26 @@
 package org.java.algorithms.arraysAlgorithms;
 
+import java.util.Scanner;
+
 public class ResizeArray {
+    /**
+     * How to resize an Array in Java
+     * */
+    public static int[] resize(int[] arr , int capacity){
+        int[] temp =new int[capacity];
+        System.out.println("size of insial Array is " + arr.length);
+        for(int i=0 ; i<arr.length ;i++){
+            temp[i]=arr[i];
+        }
+
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the size of the new Array ");
+        int capacity= scanner.nextInt();
+        System.out.println("size of the New Array is " + resize(new int[]{1,2,3,4,5},capacity).length);
+
+    }
 }
